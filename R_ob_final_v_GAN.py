d864b7087b7eb258ecebc0306d817d3c900f0598
jin mo byeon <107828109+byeonjinmo@users.noreply.github.com>
2023-11-27 13:12:39 +0900

    페널티 로직 오류 수정 3(학습 전에 추가 페널티 카운터 동작)

--- R_ob_final_v_GAN.py
+++ R_ob_final_v_GAN.py
@@ -74,7 +74,7 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
 
         def discriminator_block(in_filters, out_filters, bn=True):
-            block = [nn.Conv2d(in_filters, out_filters, 3, 2, 1), nn.LeakyReLU(0.2, inplace=True), nn.Dropout2d(0.25)]
+            block = [nn.Conv2d(in_filters, out_filters, 3, 2, 1), nn.ReLU(), nn.Dropout2d(0.25)]
             if bn:
                 block.append(nn.BatchNorm2d(out_filters, 0.8))
             return block
