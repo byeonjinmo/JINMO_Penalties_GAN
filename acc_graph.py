fa2364fbab43081c790755a52b23ff295d70ac44
진모 변 <wlsahv@naver.com>
2023-12-19 13:32:39 +0900

    정확도 비교 그래프

--- acc_graph.py
+++ acc_graph.py
@@ -2,8 +2,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # 데이터 불러오기
-data = pd.read_csv('/Users/mac/Desktop/11월16일까지 어케든 완성시켜/지표/FL0_2000(0)_resultss.csv')
-new_data = pd.read_csv('/Users/mac/Desktop/11월16일까지 어케든 완성시켜/지표/FL0_2000_300_1600_(0-1-2)0.3_0.5_resultss.csv')
+data = pd.read_csv('/Users/mac/Desktop/11월16일까지 어케든 완성시켜/지표/FL0_2000(0)_resultss.csv')
+new_data = pd.read_csv('/Users/mac/Desktop/11월16일까지 어케든 완성시켜/지표/FL0_2000_300_1600_(0-1-2)0.3_0.5_resultss.csv')
+
+# 1000 에포크까지의 데이터만 선택
+data = data.loc[data['Epoch'] <= 1000]
+new_data = new_data.loc[new_data['Epoch'] <= 1000]
 
 # 그래프 그리기
 plt.figure(figsize=(12, 6))
@@ -17,6 +21,8 @@ plt.plot(new_data['Epoch'], new_data['Average D Accuracy'], label='Proposed D Ac
 # 그래프 설정
 plt.xlabel('Epoch', fontsize=20)
 plt.ylabel('Accuracy', fontsize=20)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
 # plt.title('Comparison of Average D Accuracy over Epochs (Updated)')
 plt.legend(fontsize=20)
 plt.grid(True)
