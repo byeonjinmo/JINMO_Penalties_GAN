7e26a180162b637466e8f38525e4d82acd999e64
진모 변 <wlsahv@naver.com>
2023-12-19 14:22:24 +0900

    손실 비교 그래프

--- Loss_Graph.py
+++ Loss_Graph.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Reading the CSV file
-latest_file_path = '/Users/mac/Desktop/11월16일까지 어케든 완성시켜/지표/PGGANtraining_metrics2.csv'
+latest_file_path = '/Users/mac/Desktop/11월16일까지 어케든 완성시켜/지표/FL0_2000_300_1600_(0-1-2)0.3_0.5_resultss.csv'
 latest_data = pd.read_csv(latest_file_path)
 
 # Trimming the data to 1000 epochs
@@ -12,14 +12,14 @@ latest_data_trimmed = latest_data[latest_data['Epoch'] < 1000]
 
 # Extracting data
 latest_epochs = latest_data_trimmed['Epoch']
-generator_loss = latest_data_trimmed['Generator Loss']
-discriminator_loss = latest_data_trimmed['Discriminator Loss']
+generator_loss = latest_data_trimmed['Average G Loss']
+discriminator_loss = latest_data_trimmed['Average D Loss']
 
 # Creating a larger plot
 plt.figure(figsize=(15, 8))
 
 # Plotting the data with markers every 20 epochs
-plt.plot(latest_epochs, generator_loss, marker='o', linestyle='-', color='b', label=' Average Generator Loss', markevery=20)
+plt.plot(latest_epochs, generator_loss, marker='o', linestyle='-', color='b', label=' Average Generator Loss', markevery=10)
 plt.plot(latest_epochs, discriminator_loss,  linestyle='-', color='r', label=' Average Discriminator Loss', markevery=20)
 
 # Setting the title and labels
