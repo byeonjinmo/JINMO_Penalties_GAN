6d3e6ad424292aed03198d3beed8e6df65dc994d
진모 변 <wlsahv@naver.com>
2024-01-08 12:16:06 +0900

    손실 비교 그래프

--- acc_graph.py
+++ acc_graph.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # 데이터 불러오기
-data = pd.read_csv('/Users/mac/Desktop/11월16일까지 어케든 완성시켜/지표/FL0_2000(0)_resultss.csv')
-new_data = pd.read_csv('/Users/mac/Desktop/11월16일까지 어케든 완성시켜/지표/FL0_2000_300_1600_(0-1-2)0.3_0.5_resultss.csv')
+data = pd.read_csv('/Users/mac/Desktop/ACGAN_PST/지표/FL0_2000(0)_resultss.csv')
+new_data = pd.read_csv('/Users/mac/Desktop/ACGAN_PST/지표/FL0_2000_300_1600_(0-1-2)0.3_0.5_resultss.csv')
 
 # 1000 에포크까지의 데이터만 선택
 data = data.loc[data['Epoch'] <= 1000]
@@ -16,13 +16,13 @@ plt.figure(figsize=(12, 6))
 plt.plot(data['Epoch'], data['Average D Accuracy'], label='Tradtional D Accuracy', color='darkblue', linestyle='dotted', linewidth=2)
 
 # 새로운 데이터 (실선, 찐한 파랑색, 굵은 선)
-plt.plot(new_data['Epoch'], new_data['Average D Accuracy'], label='Proposed D Accuracy', color='darkblue', linestyle='solid', linewidth=2)
+plt.plot(new_data['Epoch'], new_data['Average D Accuracy'], label='PST D Accuracy', color='darkblue', linestyle='solid', linewidth=2)
 
 # 그래프 설정
 plt.xlabel('Epoch', fontsize=20)
 plt.ylabel('Accuracy', fontsize=20)
-plt.xticks(fontsize=20)
-plt.yticks(fontsize=20)
+plt.xticks(fontsize=17)
+plt.yticks(fontsize=17)
 # plt.title('Comparison of Average D Accuracy over Epochs (Updated)')
 plt.legend(fontsize=20)
 plt.grid(True)
